f071ffcd639210429f452675f6560b25a333d998
Gulam Faruk <golamfaruk204@gmail.com>
2021-10-10 18:45:58 +0600

    sticky menu done

--- resources/scripts js/app.js
+++ resources/scripts js/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+    // STICKY MENU 
+    $(".js__services").waypoint(function(direction){
+        if (direction == "down") {
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    })
+
+    // Mixitup (PORTFOLIO SECTION)
     var mixer = mixitup('.container')
 })
